15f679ce07bf584a8794195b2ecf61d31ad0a392
suyashpurwar1 <suyashpurwar1@gmail.com>
2025-01-10 22:49:47 +0530

    fixes

--- config/FirebaseConfig.js
+++ config/FirebaseConfig.js
@@ -1,25 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyBzxrz0nKOU5PupVtUkK7OubchOvgGWZXM",
-  authDomain: "react-native-f1eae.firebaseapp.com",
-  projectId: "react-native-f1eae",
-  storageBucket: "react-native-f1eae.appspot.com",
-  messagingSenderId: "526118451144",
-  appId: "1:526118451144:web:0093a2c18fa3bb237172fd",
-  measurementId: "G-7S1L2DEF5V",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
 export { db };
